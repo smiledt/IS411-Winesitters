037891ec0530c148fbf0e31be6432f69588173f6
smiledt <dereksmilees@gmail.com>
2020-05-11 09:54:15 -0500

    Forms

--- beer_recipes/forms.py
+++ beer_recipes/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from beer_recipes.models import BeerRecipe
+from beer_recipes.models import BeerRecipe, Hop, Fermentable, Yeast, Waters, Misc
 
 
 class RecipeForm(forms.ModelForm):
@@ -8,3 +8,38 @@ class RecipeForm(forms.ModelForm):
         model = BeerRecipe
         fields = ['name', 'description', 'brewer',
                   'batch_size', 'boil_size', 'boil_time', 'type']
+
+
+class HopForm(forms.ModelForm):
+    # TODO: Add validation here
+    class Meta:
+        model = Hop
+        exclude = ['version', 'date_submitted']
+
+
+class FermentableForm(forms.ModelForm):
+    # TODO: Add validation here
+    class Meta:
+        model = Fermentable
+        exclude = ['version', 'date_submitted']
+
+
+class YeastForm(forms.ModelForm):
+    # TODO: Add validation here
+    class Meta:
+        model = Yeast
+        exclude = ['version', 'date_submitted']
+
+
+class MiscForm(forms.ModelForm):
+    # TODO: Add validation here
+    class Meta:
+        model = Misc
+        exclude = ['version', 'date_submitted']
+
+
+class WatersForm(forms.ModelForm):
+    # TODO: Add validation here
+    class Meta:
+        model = Waters
+        exclude = ['version', 'date_submitted']
